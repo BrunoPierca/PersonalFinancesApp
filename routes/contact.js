353b14defafa91a35269133b6a5c0b8bf9c6b7cf
brunoPierca <pierca18@gmail.com>
2022-06-25 21:11:49 -0300

    Added best practices to Contact endpoint responses

--- routes/contact.js
+++ routes/contact.js
@@ -16,24 +16,27 @@ router.post("/", async (req, res) => {
 	} = req.body.data;
 	async function main() {
 		// Envio, segun la conexion del Transport
-		let contacto = await transporter.sendMail({
-			from: process.env.EMAIL, // sender address
-			to: receiverEmail, // list of receivers
-			subject: "Nuevo contacto", // Subject line
-			// Html
-			html: `<p>¡Hola! te avisamos que alguien acaba de solicitar que te contactes en: ${website}</p>
-      <div>
-      <h3>Sus datos:</h3>
-      <p>Nombre: ${name}</p>
-      <p>E-Mail: ${email}</p>
-      <p>Asunto: ${subject}</p> 
-      <p>Mensaje: ${message}</p> 
-    </div>`, // html body
-		});
+		try {
+			let contacto = await transporter.sendMail({
+				from: process.env.EMAIL, // sender address
+				to: receiverEmail, // list of receivers
+				subject: "Nuevo contacto", // Subject line
+				// Html
+				html: `<p>¡Hola! te avisamos que alguien acaba de solicitar que te contactes en: ${website}</p>
+						<div>
+						<h3>Sus datos:</h3>
+						<p>Nombre: ${name}</p>
+						<p>E-Mail: ${email}</p>
+						<p>Asunto: ${subject}</p> 
+						<p>Mensaje: ${message}</p> 
+						</div>`, // html body
+			});
+			res.status(200).json({ ok: true });
+		} catch (error) {
+			res.status(500).json({ error: error, ok: false });
+		}
 	}
 	main().catch(console.error);
-
-	res.status(200).send(name, email, message, subject);
 });
 
 module.exports = router;
